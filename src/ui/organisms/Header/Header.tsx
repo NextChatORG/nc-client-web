4e8a31ad0bac3bfd2fe756f3614f5553db9aa227
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2022-09-18 16:10:09 -0500

    feat(ui): add search results on typing

--- src/ui/organisms/Header/Header.tsx
+++ src/ui/organisms/Header/Header.tsx
@@ -1,32 +1,87 @@
-import { SearchVariables } from '@nc-core/api';
-import { useUser } from '@nc-core/hooks';
+import { SearchResponse, SearchVariables, SEARCH_QUERY } from '@nc-core/api';
+import { useLazyQuery, useUser } from '@nc-core/hooks';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
-import { Avatar, Grid, TextField } from '../../atoms';
+import { Avatar, Grid, Loading, TextField } from '../../atoms';
+import { SearchResultPreview } from '../../molecules';
 import classes from './Header.module.sass';
 
 export function Header(): JSX.Element {
-  const { control, handleSubmit } = useForm<SearchVariables>();
+  const [typing, setTyping] = useState<boolean>(false);
+
+  const { control, getValues, watch } = useForm<SearchVariables>();
   const { data } = useUser();
 
+  const [search, { data: searchData, loading: searching }] = useLazyQuery<
+    SearchResponse,
+    SearchVariables
+  >(SEARCH_QUERY);
+
   function onSubmit(variables: SearchVariables) {
-    console.log('Submit', variables);
+    return search({ variables });
   }
+
+  function handleChangeSearch(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.value.length >= 3) {
+      setTyping(true);
+    }
+  }
+
+  const searchText = watch('searchText') ?? '';
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout | null = null;
+    if (typing) {
+      timeout = setTimeout(() => {
+        onSubmit(getValues());
+        setTyping(false);
+      }, 600);
+    }
+
+    return () => {
+      if (timeout) clearTimeout(timeout);
+    };
+  }, [typing]);
 
   return (
     <header className={classes.header}>
       <Grid container alignItems="center">
         <Grid item offsetXs={3} xs={6}>
-          <TextField
-            fullWidth
-            control={control}
-            defaultValue=""
-            name="searchText"
-            placeholder="Buscar en NextChat..."
-            onSearchClick={handleSubmit(onSubmit)}
-            type="search"
-            variant="contained"
-          />
+          <div className={classes.header__search}>
+            <div className={classes.header__search__input}>
+              <TextField
+                fullWidth
+                control={control}
+                defaultValue=""
+                name="searchText"
+                placeholder="Buscar en NextChat..."
+                onChange={handleChangeSearch}
+                type="search"
+                variant="contained"
+              />
+            </div>
+            {searchText.length >= 3 && (
+              <div className={classes.header__search__results}>
+                {searching || typing ? (
+                  <div className={classes.header__search__results__text}>
+                    <Loading id="search-loading-text" text="Buscando" />
+                  </div>
+                ) : searchData && searchData.search.length > 0 ? (
+                  searchData.search.map((data, i) => (
+                    <SearchResultPreview
+                      data={data}
+                      key={`search_result_${i}`}
+                    />
+                  ))
+                ) : (
+                  <div className={classes.header__search__results__text}>
+                    Sin resultados
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
         </Grid>
         <Grid item xs={3}>
           <Grid container justifyContent="flex-end">
@@ -34,15 +89,18 @@ export function Header(): JSX.Element {
               <Grid
                 container
                 alignItems="center"
-                justifyContent="space-between"
+                justifyContent="flex-end"
+                spacing={12}
               >
-                <div className={classes.header__profile__information}>
-                  <h3
-                    className={classes.header__profile__information__username}
-                  >
-                    @{data?.username ?? ''}
-                  </h3>
-                </div>
+                <Grid item>
+                  <div className={classes.header__profile__information}>
+                    <h3
+                      className={classes.header__profile__information__username}
+                    >
+                      {data?.username ?? ''}
+                    </h3>
+                  </div>
+                </Grid>
                 <Grid item>
                   <Grid container justifyContent="flex-end">
                     <Avatar url={data?.profileImage} />
