87803dcc55be13f795587d79d1c341ddf5bffd9c
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2022-09-18 18:17:36 -0500

    feat(ui): add className prop to Content atom

--- src/ui/atoms/Content/Content.tsx
+++ src/ui/atoms/Content/Content.tsx
@@ -2,18 +2,20 @@ import clsx from 'clsx';
 import classes from './Content.module.sass';
 
 export interface ContentProps {
+  className?: string;
   fullHeight?: boolean;
   noPadding?: boolean;
 }
 
 export function Content({
   children,
+  className,
   fullHeight,
   noPadding,
 }: React.PropsWithChildren<ContentProps>): JSX.Element {
   return (
     <section
-      className={clsx(classes.content, {
+      className={clsx(className, classes.content, {
         [classes['content--fullHeight']]: fullHeight,
         [classes['content--padding']]: !noPadding,
       })}
