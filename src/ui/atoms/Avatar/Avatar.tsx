172b780959bffb3c00551e31adba4119b8013988
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2022-09-18 18:15:09 -0500

    feat(ui): add big size avatar

--- src/ui/atoms/Avatar/Avatar.tsx
+++ src/ui/atoms/Avatar/Avatar.tsx
@@ -3,7 +3,7 @@ import { PersonIcon } from '@nc-icons';
 import classes from './Avatar.module.sass';
 
 export interface AvatarProps {
-  size?: 'normal' | 'small';
+  size?: 'big' | 'normal' | 'small';
   url?: string;
 }
 
@@ -11,6 +11,7 @@ export function Avatar({ size = 'normal', url }: AvatarProps): JSX.Element {
   return (
     <div
       className={clsx(classes.avatar, {
+        [classes['avatar--big']]: size === 'big',
         [classes['avatar--normal']]: size === 'normal',
         [classes['avatar--small']]: size === 'small',
         [classes['avatar--icon']]: !url,
