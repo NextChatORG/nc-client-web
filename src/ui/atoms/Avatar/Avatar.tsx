36eae53cd6d5a572f960cbf2a945a54d40872ec0
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2022-09-18 13:08:25 -0500

    feat(ui): add Avatar atom

---
+++ src/ui/atoms/Avatar/Avatar.tsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx';
+import { PersonIcon } from '@nc-icons';
+import classes from './Avatar.module.sass';
+
+export interface AvatarProps {
+  size?: 'normal' | 'small';
+  url?: string;
+}
+
+export function Avatar({ size = 'normal', url }: AvatarProps): JSX.Element {
+  return (
+    <div
+      className={clsx(classes.avatar, {
+        [classes['avatar--normal']]: size === 'normal',
+        [classes['avatar--small']]: size === 'small',
+        [classes['avatar--icon']]: !url,
+      })}
+    >
+      {url ? <img alt="Avatar" src={url} /> : <PersonIcon size="1em" />}
+    </div>
+  );
+}
