880da0934e70b2c2375316e33dbc55f957a765f6
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2022-09-17 21:12:26 -0500

    feat(auth): decode jwt data and export it

--- src/core/hooks/useUser.ts
+++ src/core/hooks/useUser.ts
@@ -1,8 +1,15 @@
 import { JWT_TOKEN } from '@nc-core/constants/local-storage';
 import { UserContext } from '@nc-core/contexts';
 import { useContext } from 'react';
+import jwtDecode from 'jwt-decode';
+
+export interface UserData {
+  id: string;
+  username: string;
+}
 
 export interface UserHook {
+  data: UserData | null;
   logOut(): void;
   isLogged(): boolean;
 }
@@ -10,7 +17,21 @@ export interface UserHook {
 export function useUser(): UserHook {
   const { dispatch, state } = useContext(UserContext);
 
+  function getData(): UserData | null {
+    if (!state || !state.jwt || !state.jwt.length) {
+      return null;
+    }
+
+    const payload = jwtDecode<{ sub: string; username: string }>(state.jwt);
+
+    return {
+      id: payload.sub,
+      username: payload.username,
+    };
+  }
+
   return {
+    data: getData(),
     logOut() {
       if (!dispatch) return;
 
