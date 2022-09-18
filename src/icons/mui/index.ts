b0342f6672c681beab0bd9b4470f8bfbb4491f29
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2022-09-18 18:14:33 -0500

    feat(icons): add Edit and PersonAdd Material Symbols

--- src/icons/mui/index.ts
+++ src/icons/mui/index.ts
@@ -3,11 +3,13 @@ export * from './outlined';
 
 export { default as AddIcon } from './Add';
 export { default as CloseIcon } from './Close';
+export { default as EditIcon } from './Edit';
 export { default as ExpandLessIcon } from './ExpandLess';
 export { default as ExpandMoreIcon } from './ExpandMore';
 export { default as LogoutIcon } from './Logout';
 export { default as OpenInNewIcon } from './OpenInNew';
 export { default as PersonIcon } from './Person';
+export { default as PersonAddIcon } from './PersonAdd';
 export { default as SearchIcon } from './Search';
 export { default as VisibilityIcon } from './Visibility';
 export { default as VisibilityOffIcon } from './VisibilityOff';
