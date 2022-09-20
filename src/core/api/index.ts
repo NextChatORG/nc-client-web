393558c29635d2c05f81a91814db1b4c7f4a5135
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2022-09-20 17:41:34 -0500

    feat(api): add graphql subscriptions connection

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -1,13 +1,35 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  split,
+} from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { setContext } from '@apollo/client/link/context';
 import { VITE_API_URL } from '@nc-core/constants/environment';
 import { JWT_TOKEN } from '@nc-core/constants/local-storage';
+import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 export * from './auth';
 export * from './messages';
 export * from './search';
 export * from './user-friends';
 export * from './users';
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    connectionParams() {
+      const jwt = localStorage.getItem(JWT_TOKEN);
+      if (!jwt || !jwt.length) return;
+
+      return {
+        Authorization: `Bearer ${jwt}`,
+      };
+    },
+    url: 'ws://localhost:3001/graphql',
+  }),
+);
 
 const httpLink = createHttpLink({ uri: VITE_API_URL });
 
@@ -22,9 +44,22 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink),
+);
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
+  link: splitLink,
 });
 
 export default client;
