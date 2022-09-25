74035d0dfbed5983608f00a040bc2446e81057bd
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2022-09-25 00:42:07 -0500

    feat(icons): add MoreVert Material Symbol icon

--- src/icons/mui/index.ts
+++ src/icons/mui/index.ts
@@ -8,6 +8,7 @@ export { default as EditIcon } from './Edit';
 export { default as ExpandLessIcon } from './ExpandLess';
 export { default as ExpandMoreIcon } from './ExpandMore';
 export { default as LogoutIcon } from './Logout';
+export { default as MoreVertIcon } from './MoreVert';
 export { default as OpenInNewIcon } from './OpenInNew';
 export { default as PersonIcon } from './Person';
 export { default as PersonAddIcon } from './PersonAdd';
