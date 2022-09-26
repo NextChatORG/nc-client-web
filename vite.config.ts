42c17a3f4a2b716208175b8bca245ca349c8192f
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2022-09-25 19:14:44 -0500

    feat(ts): update baseUrl

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,7 +8,9 @@ export default defineConfig({
     alias: {
       '@nc-assets': fileURLToPath(new URL('./src/assets/', import.meta.url)),
       '@nc-core': fileURLToPath(new URL('./src/core/', import.meta.url)),
-      '@nc-icons': fileURLToPath(new URL('./src/icons/index.ts', import.meta.url)),
+      '@nc-icons': fileURLToPath(
+        new URL('./src/icons/index.ts', import.meta.url),
+      ),
       '@nc-ui': fileURLToPath(new URL('./src/ui/index.ts', import.meta.url)),
     },
   },
